612caa946a6f980562f9f10ceba2524ca9a32840
Ahmed <sanada927@gmail.com>
2026-02-05 23:42:25 +0200

    feat: add Account Service

---
+++ GymManagementBLL/Services/Classes/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels;
+using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace GymManagementBLL.Services.Classes
+{
+    public class AccountService(UserManager<ApplicationUser> userManager) : IAccountService
+    {
+        public ApplicationUser? ValidateUser(LoginViewModel loginViewModel)
+        {
+            var user = userManager.FindByEmailAsync(loginViewModel.Email).Result;
+
+            if (userManager.CheckPasswordAsync(user, loginViewModel.Password).Result)
+                return user;
+            else
+                return null;
+        }
+    }
+}
